19b475a8e920a91dfe440ee77e550fd07e92fa4b
Martin Abeleda <martin.abeleda@gmail.com>
2022-04-14 17:41:18 -0700

    add patterns

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -167,4 +167,13 @@ fn main() {
         _ => (),
     }    
 
+    // extra conditionals with match guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
 }
